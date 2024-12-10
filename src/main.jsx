65b07eb870e67cb7c2fb608f2cc8763e76ce35ae
yagneshprajapati <okyagnesh@gmail.com>
2024-12-10 11:47:37 +0530

    this v2

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import React, { useState } from 'react';
 import './index.css'
 import App from './App.jsx'
 
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/devnew/',
+  plugins: [react()],
+  base: '/newdev/', // This must match the repository name
 });
